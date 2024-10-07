7be573b60c8252f5dbd96f5ff84dad0d18893859
teranum <tera63614@gmail.com>
2024-10-07 20:48:14 +0900

    Update kiwoomAsync to 0.0.7; refactor ResponseData class
    Updated the `kiwoomAsync` package version in `pyproject.toml` from `0.0.6` to `0.0.7`, indicating a new release with potential new features, bug fixes, or improvements.
    
    Refactored the `ResponseData` class in `models.py` to move class-level attributes into an `__init__` method, ensuring each instance has its own set of attributes and preventing shared state issues.

--- kiwoomAsync/src/kiwoomAsync/models.py
+++ kiwoomAsync/src/kiwoomAsync/models.py
@@ -3,30 +3,20 @@
     RequestTrAsync 요청 응답 클래스
     '''
 
-    result_code = 0
-    ''' 결과 코드, 0 - 성공, 그외 - 실패, 오류메시지는 msg'''
-
-    msg = ''
-    ''' 에러메시지 '''
-
-    tr_cd = ''
-    ''' TR 코드 '''
-
-    single_datas = []
-    ''' 단일 데이터 리스트 '''
-
-    multi_datas = []
-    ''' 다중 데이터(2차원 리스트) '''
-
-    cont_key = ''
-    ''' 연속키 '''
-
-
-    in_singles = []
-    ''' 입력된 단일 데이터 리스트 '''
-
-    in_multis = []
-    ''' 입력된 다중 데이터 리스트 '''
-
-    pass
-
+    def __init__(self):
+        self.result_code = 0
+        ''' 결과 코드, 0 - 성공, 그외 - 실패, 오류메시지는 msg'''
+        self.msg = ''
+        ''' 에러메시지 '''
+        self.tr_cd = ''
+        ''' TR 코드 '''
+        self.single_datas = []
+        ''' 단일 데이터 리스트 '''
+        self.multi_datas = []
+        ''' 다중 데이터(2차원 리스트) '''
+        self.cont_key = ''
+        ''' 연속키 '''
+        self.in_singles = []
+        ''' 입력된 단일 데이터 리스트 '''
+        self.in_multis = []
+        ''' 입력된 다중 데이터 리스트 '''
